bd94f143cdedcde88ef70bfe4daa5d4e27921e18
mwyrodek <wyrodek.maciej@gmail.com>
2019-04-11 07:46:02 +0200

    fixes on exception manes

---
+++ TableElement/Exception/HeaderMissMatchException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TableElement.Exception
+{
+    [Serializable()]
+    public class HeaderMissMatchException: System.Exception
+    {
+        public HeaderMissMatchException() : base() { }
+        public HeaderMissMatchException(string message) : base(message) { }
+        public HeaderMissMatchException(string message, System.Exception inner) : base(message, inner) { }
+
+        protected HeaderMissMatchException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ TableElement/Exception/HeaderNotFoundException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TableElement.Exception
+{
+    [Serializable()]
+    public class HeaderNotFoundException : System.Exception
+    {
+        public HeaderNotFoundException() : base() { }
+        public HeaderNotFoundException(string message) : base(message) { }
+        public HeaderNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+
+        protected HeaderNotFoundException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
